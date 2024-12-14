638924ad97a63f1ebdee5b8e6c901ae6f2ce8a82
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2024-12-14 11:50:57 +0200

    token authentication works

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -20,20 +29,24 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   //console.log("blogRouter.post", body)
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title and url are required' })
   }
 
-  const user = await User.findById(body.user)
+  //const user = await User.findById(body.user)
 
   const blog = new Blog({
     title: body.title || "unknown",
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: body.user
+    user: user._id
   })
 
   await blog.validate()
